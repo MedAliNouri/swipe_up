2d7d43dd3d38e3702964c75957338c11dfec0a0d
MedALiNouri <mohamedalinouri.wrk@gmail.com>
2022-04-15 13:28:07 +0100

    one

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUriAuth:"http://192.168.1.13:3000/auth/login",
+  apiUriResetPass:"http://192.168.1.13:3000/auth/restPassword",
+  apiUrlUser:"http://192.168.1.13:3000/user",
+  apiUrlChangePass:"http://192.168.1.13:3000/auth/change_password",
+  apiUrlGetToken:"http://192.168.1.13:3000/auth/get_token_password",
 };
