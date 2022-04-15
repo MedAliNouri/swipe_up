58dede7b0ba34a875a7b832b8ab42f6a830d14cb
MedALiNouri <mohamedalinouri.wrk@gmail.com>
2022-04-15 16:17:24 +0100

    two

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUriAuth:"http://192.168.1.13:3000/auth/login",
-  apiUriResetPass:"http://192.168.1.13:3000/auth/restPassword",
-  apiUrlUser:"http://192.168.1.13:3000/user",
-  apiUrlChangePass:"http://192.168.1.13:3000/auth/change_password",
-  apiUrlGetToken:"http://192.168.1.13:3000/auth/get_token_password",
+  apiUriAuth:"https://hirukuuu.herokuapp.com/auth/login",
+  apiUriResetPass:"https://hirukuuu.herokuapp.com/auth/restPassword",
+  apiUrlUser:"https://hirukuuu.herokuapp.com/user",
+  apiUrlChangePass:"https://hirukuuu.herokuapp.com/auth/change_password",
+  apiUrlGetToken:"https://hirukuuu.herokuapp.com/auth/get_token_password",
 };
